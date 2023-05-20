d0854e046f9de6adb1b9e630b4b420e0356b6b7a
saadimalik211 <saadmali@gensosekai.com>
2023-05-19 23:34:15 -0400

    using env vars for db info

--- conveyervisionUI/conveyervisionUI/settings.py
+++ conveyervisionUI/conveyervisionUI/settings.py
@@ -82,14 +82,13 @@ WSGI_APPLICATION = 'conveyervisionUI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('cvdb'),
-        'USER': os.environ.get('cvdb-user'),
-        'PASSWORD': os.environ.get('cvdb-pw'),
-        'HOST': os.environ.get('db'),
-        'PORT': os.environ.get('5432'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
-
 
 
 # Password validation
