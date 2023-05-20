1868d952047a09bf457e1ce0da5d814fcab4b360
saadimalik211 <saadmali@gensosekai.com>
2023-05-19 23:13:50 -0400

    testing postgres setup and init

--- conveyervisionUI/conveyervisionUI/settings.py
+++ conveyervisionUI/conveyervisionUI/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,12 +73,23 @@ WSGI_APPLICATION = 'conveyervisionUI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('cvdb'),
+        'USER': os.environ.get('cvdb-user'),
+        'PASSWORD': os.environ.get('cvdb-pw'),
+        'HOST': os.environ.get('db'),
+        'PORT': os.environ.get('5432'),
     }
 }
+
 
 
 # Password validation
