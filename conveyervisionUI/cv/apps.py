fa8d2018103248224fbaf6c75b820bcb69ec9bd9
Joel <joel@wayleo.com>
2023-05-30 12:21:39 -0400

    Seeding initial yum user

--- conveyervisionUI/cv/apps.py
+++ conveyervisionUI/cv/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class CvConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cv'
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        u = User.objects.get(username="yum")
+        u.set_password("yum")
+        u.save()
+
