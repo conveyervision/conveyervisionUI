d083d8729beb2b59b9f947f02b999c02185be2b9
Joel <joel@wayleo.com>
2023-05-30 09:19:27 -0400

    Basic authentication with login and logout

--- conveyervisionUI/cv/urls.py
+++ conveyervisionUI/cv/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('login/', views.login, name='login'),
+    path('auth/', views.auth, name='auth'),
+    path('logout/', views.logout_view, name='logout'),
 ]
 
--- conveyervisionUI/cv/views.py
+++ conveyervisionUI/cv/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import FoodItem
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 def home(request):
+    return render(request, 'cv/home.html')
+
+def login(request):
+    return render(request, 'cv/login.html')
+
+@login_required(login_url="/login/")
+def dashboard(request):
     item = FoodItem.objects.first()  # gets the first FoodItem
     context = {
         'fooditem': item,
     }
-    return render(request, 'cv/home.html', context)
+    return render(request, 'cv/dashboard.html', context)
 
+def logout_view(request):
+    logout(request)
+    # Redirect to a success page.
+    return render(request, 'cv/home.html')
+
+def auth(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+                auth_login(request, user)
+        # Redirect to a success page.
+                return redirect(dashboard)
+    else:
+                # Return an 'invalid login' error message.
+                return redirect(login)
