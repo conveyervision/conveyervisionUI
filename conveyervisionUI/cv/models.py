f72f9de158ba1cfc831b3b4fec34074d7c3fc4fe
Joel <joel@wayleo.com>
2023-06-20 23:01:42 -0400

    Modifying cvspots added and location_update timestamps

--- conveyervisionUI/cv/models.py
+++ conveyervisionUI/cv/models.py
@@ -18,7 +18,7 @@ class CVSpots(models.Model):
     active = models.BooleanField(default=True)
     food = models.CharField(max_length=200)
     location = models.IntegerField()
-    added = models.DateTimeField(auto_now_add=True)
-    location_update = models.DateTimeField(auto_now=True)
+    added = models.DateTimeField()
+    location_update = models.DateTimeField()
     class Meta:
         verbose_name_plural = 'CVSpots'
